77becd8b5ba546fb66b4f7e99ec59922146edc51
smilebuz <smile_buz@163.com>
2017-02-24 17:03:04 +0800

    使用了新的数据信息

--- ECharts No.1/scripts/echarts_mission1.js
+++ ECharts No.1/scripts/echarts_mission1.js
@@ -1,35 +1,39 @@
 var myChart = echarts.init(document.getElementById("main"));
-var data = [5, 20, 36, 10, 16, 20];
+var teams = ["活塞","马刺","凯尔特人","湖人","小牛","热火","勇士","骑士"];
+var champions = [1,4,1,5,1,3,1,1];
 var option = {
   title: {
-    text: '销量信息'
+    text: '21世纪NBA各队冠军数量',
+    textStyle: {
+      fontSize: 12
+    }
   },
   tooltip: {},
   legend: {
-    data: ['销量-柱状图','销量-折线图']
+    data: ['柱状图','折线图']
   },
   xAxis: {
-    data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+    data: teams
   },
   yAxis: {},
   series: [{
-    name: '销量-柱状图',
+    name: '柱状图',
     type: 'bar',
     itemStyle: {
       normal: {
         color: '#3399FF'
       }
     },
-    data: data
+    data: champions
   },{
-    name: '销量-折线图',
+    name: '折线图',
     type: 'line',
     itemStyle: {
       normal: {
         color: '#006633'
       }
     },
-    data: data
+    data: champions
   }]
 };
 myChart.setOption(option);
