591098a074467b3eb5719f144dc51cc6aa040b08
smilebuz <smile_buz@163.com>
2017-03-15 09:26:43 +0800

    js任务五

---
+++ Javascript/task5/js/main.js
@@ -0,0 +1,130 @@
+var numbers = [10,3,7,12,11,10];
+var numberList = document.getElementById("number_list");
+var body = document.getElementsByTagName("body")[0];
+//清空队列
+function clearQueue() {
+  while (numberList.hasChildNodes()) {
+    numberList.removeChild(numberList.firstChild);
+  }
+}
+//显示数组
+function queueSimulation() {
+  for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    var list_item = document.createElement("li");
+    //var list_text = document.createTextNode(number.toString());
+    //list_item.appendChild(list_text);
+    //将数字转化为高度
+    list_item.style.height = number*5+"px";
+    numberList.appendChild(list_item);
+    console.log("display");
+    list_item.onclick = removeItem;
+  }
+}
+//点击列表项删除
+function removeItem() {
+  var listItems = numberList.children;
+  var index = 0;
+  //找出当前点击子元素位置
+  for (var i = 0; i < listItems.length; i++) {
+    if (listItems[i] === this) {
+      index = i;
+    }
+  }
+  console.log("子元素位置:"+index);
+  //从列表中和数组中分别移除点击项
+  numberList.removeChild(this);
+  numbers.splice(index,1);
+  console.log(numbers);
+}
+//左侧入
+function firstIn() {
+  var input_number = document.getElementById("input_number").value;
+  if(!checkCondition(input_number)){
+    return false;
+  }else {
+    clearQueue();
+    numbers.unshift(parseInt(input_number));
+    queueSimulation();
+  }
+}
+//右侧入
+function lastIn(number) {
+  var input_number = document.getElementById("input_number").value;
+  if (!checkCondition(input_number)) {
+    return false;
+  } else {
+    clearQueue();
+    numbers.push(parseInt(input_number));
+    queueSimulation();
+  }
+}
+//左侧出
+function firstOut() {
+  clearQueue();
+  var first_number = numbers[0];
+  numbers.shift(first_number);
+  queueSimulation();
+}
+//右侧出
+function LastOut() {
+  clearQueue();
+  var last_number = numbers[numbers.length-1]
+  numbers.pop(last_number);
+  queueSimulation();
+}
+//检查输入是否在规定范围内以及数组长度是否超过60
+function checkCondition(input_number) {
+  if (parseInt(input_number)<10||parseInt(input_number)>100) {
+    alert("当前输入值不在范围内，请输入10到100之间的数字");
+    return false;
+  }
+  if (input_number==="") {
+    alert("当前输入值为空");
+    return false;
+  }
+  if (numbers.length === 60) {
+    alert("当前队列元素已达到最大值60");
+    return false;
+  }
+  return true;
+}
+//升序重排
+function asceNumbers(number1,number2) {
+  clearQueue();
+  numbers.sort(asceSort);
+  queueSimulation();
+}
+function asceSort(number1,number2) {
+  return number1 - number2;
+}
+//降序重排
+function descNumbers(number1,number2) {
+  clearQueue();
+  numbers.sort(descSort);
+  queueSimulation();
+}
+function descSort(number1,number2) {
+  return number2 - number1;
+}
+//初始化按钮绑定事件
+function btnInit() {
+  var leftInBtn = document.getElementById("left_in_btn");
+  leftInBtn.onclick = firstIn;
+  var rightInBtn = document.getElementById("right_in_btn");
+  rightInBtn.onclick = lastIn;
+  var leftOutBtn = document.getElementById("left_out_btn");
+  leftOutBtn.onclick = firstOut;
+  var rightOutBtn = document.getElementById("right_out_btn");
+  rightOutBtn.onclick = LastOut;
+  var asceBtn = document.getElementById("resort_asce");
+  asceBtn.onclick = asceNumbers;
+  var descBtn = document.getElementById("resort_desc");
+  descBtn.onclick = descNumbers;
+}
+//
+function init() {
+  queueSimulation();
+  btnInit();
+}
+init();
